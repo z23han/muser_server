d662a667bec3d03ba664b516eeb51da5cf675ccd
z23han <zxhan2010@hotmail.com>
2020-06-13 16:36:02 -0400

    init setup for node

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => res.send('hello world'));
+
+
+
+app.listen(8080, () => {
+    console.log('listening to port 8080');
+});
